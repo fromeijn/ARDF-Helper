0c300ef8365616dcd9f676abf67ccfc6fc95086f
Floris Romeijn <romeijn.f@gmail.com>
2017-10-04 15:36:15 +0200

    aks for permission for writing to logfile

--- app/src/main/java/nl/fromeijn/ardfhelper/EnterActivity.java
+++ app/src/main/java/nl/fromeijn/ardfhelper/EnterActivity.java
@@ -28,15 +28,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
-
-import org.w3c.dom.Text;
-
 import java.util.Calendar;
 
 public class EnterActivity extends Activity implements LocationListener, SensorEventListener {
     final static int INTERVAL = 500; // 1/2s
     final static int MAX_TIME_LAST_GPS_UPDATE = 20; // 1/2s
     final static int MY_PERMISSIONS_REQUEST_GPS = 1;
+    final static int MY_PERMISSIONS_REQUEST_STORAGE = 2;
 
     private TextView latField;
     private TextView lonField;
@@ -292,19 +290,16 @@ public class EnterActivity extends Activity implements LocationListener, SensorE
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_enter, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
+        // TODO: 4-10-17 rewrite to switch
+
         if (id == R.id.open_maps) {
             startActivity(new Intent(EnterActivity.this, MapsActivity.class));
             return true;
@@ -341,9 +336,7 @@ public class EnterActivity extends Activity implements LocationListener, SensorE
                                            String permissions[], int[] grantResults) {
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_GPS: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
                     locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
                     Criteria criteria = new Criteria();
@@ -351,16 +344,9 @@ public class EnterActivity extends Activity implements LocationListener, SensorE
                     Location location = locationManager.getLastKnownLocation(provider);
                     onLocationChanged(location);
 
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
                 }
-                return;
+                break;
             }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
         }
     }
 
--- app/src/main/java/nl/fromeijn/ardfhelper/ShowFoxLog.java
+++ app/src/main/java/nl/fromeijn/ardfhelper/ShowFoxLog.java
@@ -1,22 +1,24 @@
 package nl.fromeijn.ardfhelper;
 
-import android.content.Context;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
-import java.util.Arrays;
 import java.util.List;
 import android.widget.ArrayAdapter;
 import android.app.ListActivity;
 import android.widget.Toast;
 
-public class ShowFoxLog extends ListActivity  {
+import static nl.fromeijn.ardfhelper.EnterActivity.MY_PERMISSIONS_REQUEST_STORAGE;
 
+public class ShowFoxLog extends ListActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,61 +44,64 @@ public class ShowFoxLog extends ListActivity  {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_show_fox_log, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        switch (id){
-            case R.id.clear_log:{
+        switch (id) {
+            case R.id.clear_log: {
                 // TODO: 2-10-17 ask to confirmation 
                 FoxLog.deleteAll(FoxLog.class);
                 Toast.makeText(ShowFoxLog.this, "Log Cleared", Toast.LENGTH_LONG).show();
                 finish(); // go back to EnterActivity
                 break;
             }
-            case R.id.FoxLog_to_file:{
-                
-                String state = Environment.getExternalStorageState();
-                if (!Environment.MEDIA_MOUNTED.equals(state)) {
-                    // TODO: 2-10-17 ask for permission 
-                    Toast.makeText(ShowFoxLog.this, "ERROR: Can't write file, no permission!", Toast.LENGTH_LONG).show();
-                }
+            case R.id.FoxLog_to_file: {
 
-                String filename = "ARDF-Helper.log";
+                if (ContextCompat.checkSelfPermission(ShowFoxLog.this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(ShowFoxLog.this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            MY_PERMISSIONS_REQUEST_STORAGE);
+                } else {
 
-                try {
-                    File path = new File(Environment.getExternalStorageDirectory(), "ARDF-Helper");
-                    if (!path.exists()) {
-                        path.mkdirs();
+                    String state = Environment.getExternalStorageState();
+                    if (!Environment.MEDIA_MOUNTED.equals(state)) {
+                        Toast.makeText(ShowFoxLog.this, "ERROR: Can't write file, not mounted?!?", Toast.LENGTH_LONG).show();
                     }
-                    File file = new File(path, filename);
-                    FileWriter writer = new FileWriter(file);//, true);
-                    writer.append("fox,lat,lon,angle\n");
-                    List<FoxLog> foxlogs = FoxLog.listAll(FoxLog.class);
-                    for (FoxLog foxlog : foxlogs ){
-                        writer.append(foxlog.toString());
-                        writer.append("\n");
+
+                    String filename = "ARDF-Helper.log";
+
+                    try {
+                        File path = new File(Environment.getExternalStorageDirectory(), "ARDF-Helper");
+                        if (!path.exists()) {
+                            path.mkdirs();
+                        }
+                        File file = new File(path, filename);
+                        FileWriter writer = new FileWriter(file);//, true);
+                        writer.append("fox,lat,lon,angle\n");
+                        List<FoxLog> foxlogs = FoxLog.listAll(FoxLog.class);
+                        for (FoxLog foxlog : foxlogs) {
+                            writer.append(foxlog.toString());
+                            writer.append("\n");
+                        }
+                        writer.flush();
+                        writer.close();
+                        Toast.makeText(ShowFoxLog.this, "Log successful written to file!", Toast.LENGTH_LONG).show();
+                    } catch (Exception e) {
+                        Toast.makeText(ShowFoxLog.this, "ERROR: Can't write file!", Toast.LENGTH_LONG).show();
+                        e.printStackTrace();
                     }
-                    writer.flush();
-                    writer.close();
-                    Toast.makeText(ShowFoxLog.this, "Log successful written to file!", Toast.LENGTH_LONG).show();
-                } catch (Exception e) {
-                    Toast.makeText(ShowFoxLog.this, "ERROR: Can't write file!", Toast.LENGTH_LONG).show();
-                    e.printStackTrace();
+                    break;
                 }
-                break;
             }
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
